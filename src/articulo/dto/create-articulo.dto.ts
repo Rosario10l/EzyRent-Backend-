116e7a5dbdb7521bb76ebf75d35ba38f4a92140e
uziel <utti222033@utvco.edu.mx>
2025-07-16 08:41:31 -0600

    dtos articulos create & update modificado

--- src/articulo/dto/create-articulo.dto.ts
+++ src/articulo/dto/create-articulo.dto.ts
@@ -1,1 +1,30 @@
-export class CreateArticuloDto {}
+import { IsNotEmpty, isNumber, IsNumber, IsOptional, IsString , Length, Max} from "class-validator";
+
+export class CreateArticuloDto {
+    @Length(5,30)
+    @IsString()
+    @IsNotEmpty()
+    nombre:string
+
+    @Length(5,40)
+    @IsString()
+    @IsNotEmpty()
+    description:string
+
+    @IsNumber()
+    @IsNotEmpty()
+    precio:number
+
+    @IsNumber()
+    @IsNotEmpty()
+    cantidad_total:number
+
+    @IsNumber()
+    @IsNotEmpty()
+    cantidad_disponible:number
+
+    @IsString()
+    @IsNotEmpty()
+    imagen_url:string
+
+}
