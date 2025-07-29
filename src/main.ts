6d4f7fb2dbfa407ea1212f231eca894396d1f702
Beatriz <utti222031@utvco.edu.mx>
2025-07-29 09:40:20 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+
+   // Permitir CORS para http://localhost:8100
+  app.enableCors({
+    origin: 'http://localhost:8100', // o '*' para permitir todos (menos seguro)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+ await app.listen(process.env.PORT ?? 3000);
 }
+
+
+
 bootstrap();
