af50883c0914d48d89dac50113a3781954d26662
uziel <utti222033@utvco.edu.mx>
2025-07-30 01:06:23 -0600

    controller y service modificado para conectar con frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin:'http://localhost:8100',
+    methods: 'GET, HEAD, PU, PATCH, POST, DELETE'
+  })
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
 
