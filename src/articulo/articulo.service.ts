c48096dea8c7f953863fa7e0ad17b54dd9f94989
uziel <utti222033@utvco.edu.mx>
2025-08-01 10:12:29 -0600

    articulo service modificado

--- src/articulo/articulo.service.ts
+++ src/articulo/articulo.service.ts
@@ -1,26 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException} from '@nestjs/common';
 import { CreateArticuloDto } from './dto/create-articulo.dto';
 import { UpdateArticuloDto } from './dto/update-articulo.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Articulo } from './entities/articulo.entity';
+import { log } from 'console';
 
 @Injectable()
 export class ArticuloService {
-  create(createArticuloDto: CreateArticuloDto) {
-    return 'This action adds a new articulo';
+  constructor(
+    @InjectRepository(Articulo)
+    private _ArticuloRepo:Repository<Articulo>
+  ){}
+  
+  
+  async createarticulo(createDto: CreateArticuloDto) {
+    try{
+      const newArticulo=this._ArticuloRepo.create(createDto);
+      await this._ArticuloRepo.save(newArticulo)
+      return newArticulo
+    }catch(error){
+      console.error('error al crear articulo:', error)
+      throw new InternalServerErrorException('no se pudo crear el articulo');
+    }
   }
 
-  findAll() {
-    return `This action returns all articulo`;
+  async findAll() {
+    try{
+      return await this._ArticuloRepo.find()
+    }catch(error){
+      console.error('Error al bsuacr los articulos:',error)
+      throw new NotFoundException(`Articulos no encontrados`)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} articulo`;
+  async findArticulo(id: number) {
+try{
+    const articulo=await this._ArticuloRepo.findOneBy({id});
+    return articulo;
+    }catch(error){
+      console.error('Error al buscar el articulo:', error); 
+      throw new NotFoundException(`Articulo con el id: ${id} no encontrado`)
+    }  }
+
+
+  async updateArticulo(id: number, updateArticuloDto: UpdateArticuloDto) {
+try{
+    const articulo = await this._ArticuloRepo.findOneBy({ id });
+  if(!articulo){
+      throw new NotFoundException(`mensaje`);
+    }
+    const updateArticulo = this._ArticuloRepo.merge(
+      articulo,
+      updateArticuloDto,
+    );
+    return await this._ArticuloRepo.save(updateArticulo);
+  }catch(error){
+    if (error instanceof NotFoundException) {
+    throw error;
+  }
+   console.error(`Error al actualizar el articulo con id ${id}:, error`);
+      throw new InternalServerErrorException(
+        'No se pudo actualizar el articulo.',
+  );
+  }  
+}
+
+
+  async remove(id: number) {
+    try {
+      const articulo = await this._ArticuloRepo.findOneBy({ id });
+      if (!articulo) {
+        throw new NotFoundException(`El artículo con ID: ${id} no fue encontrado.`);
+      }
+      await this._ArticuloRepo.remove(articulo); 
+      return { message: `El artículo con ID: ${id} se ha eliminado exitosamente.` };
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(`Ocurrió un error al eliminar el artículo con ID ${id}: ${error.message || error}`);
+    }
   }
 
-  update(id: number, updateArticuloDto: UpdateArticuloDto) {
-    return `This action updates a #${id} articulo`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} articulo`;
-  }
+  
 }
