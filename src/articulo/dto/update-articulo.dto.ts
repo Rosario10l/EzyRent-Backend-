6aa95823d4ef7abc6cb48cc42c8510e5c9d17a59
uziel <utti222033@utvco.edu.mx>
2025-08-01 10:17:08 -0600

    dto update articulos modificado

--- src/articulo/dto/update-articulo.dto.ts
+++ src/articulo/dto/update-articulo.dto.ts
@@ -1,4 +1,30 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateArticuloDto } from './create-articulo.dto';
+import { IsNumber, IsOptional, IsString , Length, Max} from "class-validator";
 
-export class UpdateArticuloDto extends PartialType(CreateArticuloDto) {}
+export class UpdateArticuloDto{
+    @Length(5,30)
+    @IsString()
+    @IsOptional()
+    nombre:string
+    
+    @Length(5,40)
+    @IsString()
+    @IsOptional()
+    description:string
+    
+    @IsNumber()
+    @IsOptional()
+    precio:number
+    
+    @IsNumber()
+    @IsOptional()
+    cantidad_total:number
+    
+    @IsNumber()
+    @IsOptional()
+    cantidad_disponible:number
+    
+    @IsString()
+    @IsOptional()
+    imagen_url:string
+    
+}
